6e33ec5c74294497e49f3ae0d57d3eacd33735ed
Steven Almeroth <steven.almeroth@gmail.com>
2025-03-23 10:19:50 -0400

    shell script

---
+++ anthem.sh
@@ -0,0 +1,9 @@
+echo ==========================================================================
+date
+cd /srv/statira
+pwd
+ls -l venv/bin/activate
+. venv/bin/activate
+python --version
+ls -l statira/anthem.py
+python -X dev statira/anthem.py
