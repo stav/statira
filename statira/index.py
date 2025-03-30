bfc0b0f18ee2094d74d63f78be73bdd6568f3b8f
Steven Almeroth <steven.almeroth@gmail.com>
2025-03-30 16:02:14 -0400

    help sample

--- statira/index.py
+++ statira/index.py
@@ -2,11 +2,31 @@ from fasthtml.common import (
     Button,
     Div,
     Form,
-    H3,
+    H4,
     Input,
     Label,
     P,
+    Pre,
     Titled,
+)
+
+help = (
+    P(
+        """
+        This tool allows you to upload a CSV file containing Medicaid data.
+        You can choose to parse the file contents and check eligibility on Anthem.
+        """
+    ),
+    H4("Example CSV File:"),
+    Pre(
+        """
+First Name,Last Name,DOB,MBI,SSN,Medicaid
+John,Doe,01/01/1951,123456789,123-45-1111,987654321
+Jane,Doe,02/02/1952,234567891,987-65-2222,
+John,Smith,01/01/1953,345678912,,987654321
+Jane,Smith,02/02/1954,456789123,,
+"""
+    ),
 )
 
 
@@ -14,19 +34,17 @@ def page():
 
     return Titled(
         "Medicaid Eligibility Checker",
-        H3("Upload CSV file"),
         Form(
             Input(
                 type="file",
                 name="file",
                 accept=".csv",
-                required=True,
-                onchange="document.getElementById('content').innerHTML = '';",
+                onchange="document.getElementById('results_content').innerHTML = '';",
             ),
             P(
                 Label(
                     "Parse file contents:",
-                    Input(type="checkbox", name="parse"),
+                    Input(type="checkbox", name="parse", required=True, checked=True),
                 ),
                 Label(
                     "Check eligibility on Anthem:",
@@ -34,10 +52,11 @@ def page():
                 ),
                 style="display: flex; gap: 1em;",
             ),
-            Button("Upload", type="submit"),
+            Button("Upload", type="submit", style="margin-bottom: 0"),
             enctype="multipart/form-data",
             hx_post="/upload",
-            hx_target="#content",
+            hx_target="#results_content",
         ),
-        Div(id="content"),
+        Div(help, id="results_content"),
+        style="display: flex; flex-direction: column; gap: 1em; padding: 2em; max-width: 600px; margin: auto;",
     )
--- statira/upload.py
+++ statira/upload.py
@@ -1,16 +1,24 @@
-from starlette.requests import Request
-from parse import parse_csv, parse_display
+import io
 
+from starlette.requests import Request
+from starlette.datastructures import UploadFile
+
+from parse import parse_csv, parse_display
 from sserver import anthem
 
 
 async def post(request: Request):
     form = await request.form()
     file = form.get("file")
+    # Process the sample CSV file if no file is provided
     if not file:
-        return "No file provided."
-    # if not file.filename.lower().endswith(".csv"):
-    #     return "File name must end with 'csv' extension."
+        file = UploadFile(
+            filename="sample.csv",
+            headers={"content-type": "text/csv"},
+            file=io.BytesIO(sample_csv_file_contents.encode("utf-8")),
+            size=len(sample_csv_file_contents),
+        )
+    # Validate the file
     if file.content_type != "text/csv":
         return "Invalid content type. Only CSV files are allowed."
     if file.size > 10 * 1024 * 1024:
@@ -30,10 +38,13 @@ async def post(request: Request):
             json_display = parse_display(datas)
             messages.append(json_display)
 
-    else:
-        if form.get("anthem"):
-            messages.append(
-                "Anthem eligibility check is not available without parsing the file."
-            )
-
     return messages
+
+
+sample_csv_file_contents = (
+    """\
+First Name,Last Name,DOB,MBI,SSN,Medicaid
+John,Doe,01/01/1951,123456789,123-45-1111,987654321
+Jane,Doe,02/02/1952,234567891,987-65-2222,
+John,Smith,01/01/1953,345678912,,987654321
+""")
