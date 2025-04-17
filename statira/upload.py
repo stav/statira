fb5eb796d333d498c83adc2d664f040de980c114
Steven Almeroth <steven.almeroth@gmail.com>
2025-04-16 23:34:45 -0400

    validations

--- statira/upload.py
+++ statira/upload.py
@@ -8,9 +8,12 @@ from parse import parse_csv, parse_display, parse_message
 
 
 async def post(request: Request):
+    messages = []
+
     form = await request.form()
     file = form.get("file")
     paste = form.get("paste")
+    anthem = form.get("anthem")
     if not file:
         # Check if pasted data from the textarea is provided
         if not paste:
@@ -24,20 +27,35 @@ async def post(request: Request):
                 file=io.BytesIO(paste.encode("utf-8")),
                 size=len(paste),
             )
-    # Validate the file
-    if file.content_type != "text/csv":
-        return "Invalid content type. Only CSV files are allowed."
-    if file.size > 10 * 1024 * 1024:
-        return "File is too large. Maximum size is 10 MB."
-    if file.size == 0:
-        return "File is empty."
 
-    messages = []
-
+    # Parse the data
     content, display, ok = parse_csv(file)
+
+    # Validate the inputs
+    if form.get("file") and paste:
+        data = dict(message="Pasted data ignored. Clear file input to upload pasted data.")
+        messages.append(parse_message(data))
+        ok = False
+    if file.content_type != "text/csv":
+        data = dict(message="Invalid content type. Only CSV files are allowed.")
+        messages.append(parse_message(data))
+        ok = False
+    if file.size > 10 * 1024 * 1024:
+        data = dict(message="File is too large. Maximum size is 10 MB.")
+        messages.append(parse_message(data))
+        ok = False
+    if file.size == 0:
+        data = dict(message="File is empty.")
+        messages.append(parse_message(data))
+        ok = False
+    if anthem and not ok:
+        data = dict(message="Data not sent to Anthem because it requires valid CSV.")
+        messages.append(parse_message(data))
+
+    # Now we can add the output of the processing for each response record
     messages.append(display)
 
-    if ok and form.get("anthem"):
+    if anthem and ok:
         process = start(content)
         # datas is a list of dicts from a comprehension that drains the start generator
         datas = [p async for p in process]
